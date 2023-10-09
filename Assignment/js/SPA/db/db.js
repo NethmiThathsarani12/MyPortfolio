7dbb458b8748ef012b705fb63aacf4db67401cdc
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2023-10-09 11:59:49 +0530

    done customer

--- Assignment/js/SPA/db/db.js
+++ Assignment/js/SPA/db/db.js
@@ -1,20 +1,7 @@
 var customerDB = [
-    {id: "C00-001", name: "Sachin Thamalsha", address: "Mathara", salary: 100000},
-    {id: "C00-002", name: "Ranjith Perera", address: "Panadura", salary: 200000},
-    {id: "C00-003", name: "Kavindu Perera", address: "Panadura", salary: 300000}
-];
+    {id: "C00-001", name: "Savindi Rashmika", address: "Matara", salary: 5000},
+    {id: "C00-002", name: "Nethmi Thathsarani", address: "Matara", salary: 5000},
+    {id: "C00-003", name: "Lasini Prebashi", address: "Ahangam", salary: 4000},
+    {id: "C00-004", name: "Kalana Nethmina", address: "Tissa", salary: 10000}
 
-var itemDB = [
-    {code:"I00-001",description:"Lux",qtyOnHand: 100,unitPrice: 145.00},
-    {code:"I00-002",description:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
-    {code:"I00-003",description:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
-];
-
-var orderDB = [
-    {oid:"OID-001", date:"2023/10/06", customerID:"C00-001",
-        orderDetails:[
-            {oid:"OID-001", code:"I00-001", qty:10, unitPrice:145.00},
-            {oid:"OID-001", code:"I00-002", qty:2, unitPrice:345.00}
-        ]
-    }
 ];
