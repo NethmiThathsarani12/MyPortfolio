33d2b7df8bd2f2a3b22edcde306b2f0479dd5117
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2023-10-09 12:53:23 +0530

    update item modele and bd

--- Assignment/js/SPA/db/db.js
+++ Assignment/js/SPA/db/db.js
@@ -7,5 +7,7 @@ var customerDB = [
 ];
 
 var itemDB = [
-    {}
+    {code: "I00-001" , description:"soap" , qtyOnHand: 10 , unitPrice: 200},
+    {code: "I00-002" , description:"cream" , qtyOnHand: 100 , unitPrice: 400},
+    {code: "I00-003" , description:"biscuit" , qtyOnHand: 20 , unitPrice: 250}
 ];
