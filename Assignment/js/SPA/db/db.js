f00bac776cc653a84ff2977528bb24b8bc6503b9
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2023-10-13 17:01:13 +0530

    add oder db class

--- Assignment/js/SPA/db/db.js
+++ Assignment/js/SPA/db/db.js
@@ -12,3 +12,7 @@ var itemDB = [
     {code:"I00-002",description:"Sunlight",unitPrice: 345.00,qtyOnHand: 150},
     {code:"I00-003",description:"Light Boy",unitPrice: 245.00,qtyOnHand: 400}
 ];
+
+
+var orderDB = [];
+var purchaseOrderDetailsDB=[];
